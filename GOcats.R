aa91374999ea6872bbfb1d8e75cf437887fd7303
hyginn <boris.steipe@utoronto.ca>
2018-11-13 15:20:23 -0500

    Setup project framework - files and folders

---
+++ GOcats.R
@@ -0,0 +1,52 @@
+# GOcats.R
+#
+# Purpose: see http://steipe.biochemistry.utoronto.ca/abc/index.php/ABC-INT-GO_categories
+# Version: 0.1
+# Date:    2018-11
+# Author:  Boris Steipe <boris.steipe@utoronto.ca>
+#
+# ToDo:
+# Notes:
+#
+# ==============================================================================
+
+
+# ====  PARAMETERS  ============================================================
+# Define and explain all parameters. No "magic numbers" in your code below.
+
+GOfile <- "../go-basic.obo"
+
+
+# ====  PACKAGES  ==============================================================
+# Load all required packages.
+
+# if (! require(seqinr, quietly=TRUE)) {
+#   install.packages("seqinr")
+#   library(seqinr)
+# }
+# Package information:
+#  library(help = seqinr)       # basic information
+#  browseVignettes("seqinr")    # available vignettes
+#  data(package = "seqinr")     # available datasets
+
+
+
+
+# ====  FUNCTIONS  =============================================================
+
+#source("<myUtilityFunctionsScript.R>")
+
+
+# ====  PROCESS  ===============================================================
+
+# How many terms in the ontology?
+
+
+
+
+
+# ====  TESTS  =================================================================
+# Enter your function tests here...
+
+
+# [END]
---
+++ functionTemplate.R
@@ -0,0 +1,36 @@
+# functionTemplate.R
+#
+# Purpose:  (General)
+#
+# ToDo:
+# Notes:
+#
+# ==============================================================================
+
+myFunction <- function(a, b=1) {
+	# Purpose:
+	#     Describe ...
+    # Version:
+    # Date:
+    # Author:
+    #
+    # Parameters:
+	#     a: ...
+	#     b: ...
+	# Value:
+	#     result: ...
+
+	# code ...
+
+	return(result)
+}
+
+
+# ====  TESTS  =================================================================
+# Enter your function tests here...
+
+if (FALSE) {
+  # test ...
+}
+
+# [END]
